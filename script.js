a957c186415e361e566e0b70fd08ee1bcd9f1ca6
BettyOmv <beomv8076@uib.no>
2026-03-13 10:52:55 +0100

    endringer
    lagte seperat html til hver oppg. bruker funksjon fra oppg 1b (maren sin kode) så foreløpig funker den ikke

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function splitLines(text) {
   return text.split("\\n").filter(line => line !== "");
 }
+
+function pairSums() {
+  const input = document.getElementById("input").value.split(",");
+  const tall = toNumbers(input)// Maren sin kode fra 1b
+  const output = document.getElementById("output");
+  output.innerHTML = "";
+
+  for (let i = 0; i < tall.length - 1; i++) {
+    const a = tall[i];
+    const b = tall[i + 1];
+    output.innerHTML += "<li>" + a + " + " + b + " = " + (a + b) + "</li>";
+  }
+}
