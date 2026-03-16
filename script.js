9379773ddedfe094ebc2dda872b099bfe36d27fe
marvahan <mahan4300@uib.no>
2026-03-16 12:50:38 +0100

    Lagd oppgave 1.b og 2, kommer sikkert til å endre litt på de senre

--- script.js
+++ script.js
@@ -14,3 +14,43 @@ function pairSums() {
     output.innerHTML += "<li>" + a + " + " + b + " = " + (a + b) + "</li>";
   }
 }
+
+//Oppgave 1.b
+function toNumbers() {
+  const input = document.getElementById("inputNumbers").value;
+  const stringArray = input.split(",");
+  const numbers = stringArray.map(Number);
+  document.getElementById("resultat").textContent = "Oppfanget og brukbare nummer: " + numbers;
+}
+
+//Oppgave 2
+function leggTilGjoremal() {
+  const input = document.getElementById("gjoremalInput");
+  const tekst = input.value.trim();
+  if (tekst === "") return;
+
+  const li = document.createElement("li");
+
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  checkbox.onchange = function() {
+    span.classList.toggle("ferdig");
+  };
+
+  const span = document.createElement("span");
+  span.textContent = " " + tekst;
+
+  const fjernKnapp = document.createElement("button")
+  fjernKnapp.textContent = "Fjern";
+  fjernKnapp.onclick = function() {
+    li.remove();
+  };
+
+  li.appendChild(checkbox);
+  li.appendChild(span);
+  li.appendChild(fjernKnapp);
+
+  document.getElementById("GMliste").appendChild(li);
+
+  input.value = "";
+}
